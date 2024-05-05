88aa456bdd0264f6e375ea4f4b5e6749315f38c7
Dan Fortinho <fortinhodanvhamba@gmail.com>
2024-05-05 16:14:10 +0200

    db connect and create custom function for CRUD SQL QUERY

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include ("./includes/header.php")?>
+<?php include ("./includes/navbar.php")?>
+    <div id="layoutSidenav_nav">
+    <?php include ("./includes/sidebar.php")?>
+    </div>
+    <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <?php include ("./pages/dashboard.php")?>
+                    </div>
+<?php include ("./includes/footer.php")?>
---
+++ pages/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+ include ("./includes/widget.php");
+
+ include ("./includes/chart.php");
+
+ include ("./includes/table.php")
+ 
+ 
+ 
+ ?>
